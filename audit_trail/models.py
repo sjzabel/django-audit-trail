12fad3a2dce96978585a55a30c22e86f6a0923c7
Stephen Zabel <szabel@q2software.com>
2011-04-12 11:08:55 -0500

    Update

--- audit_trail/models.py
+++ audit_trail/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from duct_tape.models import TimeStampedModelMixin
 
-from duct_tape.models import TimeStampedModelBase
-
-class AuditLog(TimeStampedModelBase):
+class AuditLog(TimeStampedModelMixin,models.Model):
     #fields that allow for a generic relation
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
